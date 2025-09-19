b2dd195a301c7578e838126c9fcb8ecec4ca0c2b
camararicardo-dev <202507039351@alunos.estacio.br>
2025-09-18 21:52:59 -0300

    Inclusão de quebras de linha.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,35 +13,35 @@ int main() {
     // Seção de inserção de dados pelo usuário
 
         //Primeiro pais
-        printf("Cadastro das informações da carta do primeiro país, insira as infromações conforme solicitadas na tela!");
+        printf("Cadastro das informações da carta do primeiro país, insira as infromações conforme solicitadas na tela! \n");
 
-        printf("Digite a população do país da primeira carta:");
+        printf("Digite a população do país da primeira carta:\n");
         scanf("%d",&populacaop1);
 
-        printf("Digite a quantidade de pontos turisticos do  país da primeira carta:");
+        printf("Digite a quantidade de pontos turisticos do  país da primeira carta:\n");
         scanf("%d",&pontoturisticop1);
 
-        printf("Digite a area do país da  primeira carta:");
+        printf("Digite a area do país da  primeira carta:\n");
         scanf("%f",&areap1);
 
-        printf("Digite o PIB do país da primeira carta:");
+        printf("Digite o PIB do país da primeira carta:\n");
         scanf("%f", &pibp1);
 
 
          //Segundo país
-        printf("Cadastro das informações da carta do segundo país, insira as infromações conforme solicitadas na tela!");
+        printf("Cadastro das informações da carta do segundo país, insira as infromações conforme solicitadas na tela!\n");
 
 
-        printf("Digite a população do país da segunda carta:");
+        printf("Digite a população do país da segunda carta:\n");
         scanf("%d",&populacaop2);
 
-        printf("Digite a quantidade de pontos turisticos do  país da segunda carta:");
+        printf("Digite a quantidade de pontos turisticos do  país da segunda carta:\n");
         scanf("%d", &pontoturisticop2);
 
-        printf("Digite a area do país da  segunda carta:");
+        printf("Digite a area do país da  segunda carta:\n");
         scanf("%f",&areap2);
 
-        printf("Digite o PIB do país da segunda carta:");
+        printf("Digite o PIB do país da segunda carta:\n");
         scanf("%f", &pibp2);
 
         // Seção de exibição dos dados coletados;
@@ -50,7 +50,7 @@ int main() {
 
         // Carta 1
 
-        printf("Dados da primeira carta:");
+        printf("Dados da primeira carta:\n");
         printf("A população é de:%d \n", populacaop1);
         printf("Possui %d pontos turisticos \n", pontoturisticop1);
         printf("Possui area de %f \n", areap1);
@@ -59,7 +59,7 @@ int main() {
 
         // Carta 2
 
-        printf("Dados da segunda carta:");
+        printf("Dados da segunda carta:\n");
         printf("A população é de:%d \n", populacaop2);
         printf("Possui %d pontos turisticos \n", pontoturisticop2);
         printf("Possui area de %f \n", areap2);
